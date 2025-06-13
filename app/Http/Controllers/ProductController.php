8042d4263c25ab3756d38a7670f8a3f990d55e7a
fikserr <azizxon7777777@gmail.com>
2025-06-13 14:09:48 +0500

    aaaaaaaaa

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -50,6 +50,7 @@ class ProductController extends Controller
     public function index(Request $request)
     {
         $search = $request->input('search');
+        $perPage = $request->input('per_page', 10); // default 10 ta
 
         $products = Product::when($search, function ($query, $search) {
             $query->where('product_name', 'like', "%{$search}%")
