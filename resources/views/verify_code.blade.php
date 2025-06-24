cd7cf1561cf2e6df5bacd1a0a9e985114eb24c5d
fikserr <azizxon7777777@gmail.com>
2025-06-24 15:51:33 +0500

    add verify

--- resources/views/verify_code.blade.php
+++ resources/views/verify_code.blade.php
@@ -1,13 +1,23 @@
 <!DOCTYPE html>
 <html>
-<head>
-    <meta charset="UTF-8">
-    <title>Tasdiqlash kodi</title>
-</head>
-<body>
-    <h1>Salom!</h1>
-    <p>Sizning tasdiqlash kodingiz:</p>
-    <h2>{{ $code }}</h2>
-    <p>Iltimos, bu kodni ro‘yxatdan o‘tishda kiriting.</p>
+
+<body style="font-family: Arial, sans-serif;">
+    <div style="max-width: 600px; padding: 20px; border-radius: 8px; border: 1px solid #eaeaea;">
+        <h2 style="color: #333">👋 Salom!</h2>
+        <p>Sizning tasdiqlash kodingiz:</p>
+        <div style="
+            font-size: 32px;
+            font-weight: bold;
+            color: #007BFF;
+            background: #f9f9f9;
+            padding: 15px;
+            border-radius: 5px;
+            text-align: center;">
+            {{ $rawCode }}
+        </div>
+        <p>Ushbu kod <strong>10 daqiqa</strong> davomida amal qiladi. Iltimos, uni boshqa hech kim bilan ulashmang!</p>
+        <p>Rahmat, <br> <strong>{{ config('app.name') }}</strong></p>
+    </div>
 </body>
+
 </html>
