ff4c295e6388ae69c094e36c5a1103107d9a5e76
ADMIN <azizxon7777777@gmail.com>
2025-10-18 15:44:51 +0500

    add deploy

---
+++ public/build/assets/chevron-left-DmNVoiE1.js
@@ -0,0 +1,7 @@
+import{c as o}from"./app-0nnjcZrn.js";
+/**
+ * @license lucide-react v0.511.0 - ISC
+ *
+ * This source code is licensed under the ISC license.
+ * See the LICENSE file in the root directory of this source tree.
+ */const p=o("chevron-left",[["path",{d:"m15 18-6-6 6-6",key:"1wnfg3"}]]);export{p as C};
